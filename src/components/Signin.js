d91d6bb3f3a2960206a10b6872c1092f12db5f3b
saurabh638 <sharmasp2207@gmail.com>
2022-08-31 21:34:52 +0530

    sign-in-page-ui-improv

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -3,27 +3,27 @@ import { Link } from "react-router-dom";
 const Signin = () => {
   return (
     <div className="h-full">
-      <div className="w-11/12 mx-auto py-5">
+      <div className="w-8/12 mx-auto py-5">
         {/* Navlinks */}
         <div className="flex flex-nowrap gap-2 text-lg">
           <Link to="/" className="underline">
             Home
           </Link>
           <p>></p>
-          <Link to="/signin">Signin</Link>
+          <Link to="/signin">Sign in</Link>
         </div>
         {/* Signin Heading */}
-        <div className="text-left pt-10 pb-2 border-red-700 border-b">
-          <h1 className="text-[38px] font-medium">Sign in to the IRCC portal</h1>
+        <div className="text-left pb-2">
+          <h1 className="h1-page-title">Sign in to the IRCC portal</h1>
         </div>
         {/* Form */}
         <form className="text-left py-5">
           <div>
             {/* Email Input */}
             <div>
-              <p className="text-xl font-bold">
-                <span className="text-red-600 pr-2">*</span>Email address
-                <span className="text-red-600 pl-2">(required)</span>
+              <p className="text-xl font-bold form-input">
+                <span className="pr-2 form-input red-bold">*</span>Email address
+                <span className="pl-2 form-input red-bold">(required)</span>
               </p>
               <input
                 class="peer"
@@ -33,9 +33,9 @@ const Signin = () => {
             </div>
             {/* Password Input */}
             <div className="mt-8">
-              <p className="text-xl font-bold">
-                <span className="text-red-600 pr-2">*</span>Password
-                <span className="text-red-600 pl-2">(required)</span>
+              <p className="text-xl font-bold form-input">
+                <span className="pr-2 red-bold">*</span>Password
+                <span className="pl-2 red-bold">(required)</span>
               </p>
               <input
                 type="password"
@@ -44,7 +44,7 @@ const Signin = () => {
             </div>
             {/* Submit Button */}
             <div className="mt-10">
-              <Link to="/terms" className="w-2/4 text-xl py-4 px-4 bg-[#335075] text-white rounded-lg">
+              <Link to="/terms" className="signin-button text-xl py-4 px-4 bg-[#335075] text-white rounded-lg">
                 Sign in
               </Link>
             </div>
